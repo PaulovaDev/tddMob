c7dfd5b459e2ce7b16430c4ac7c5810044c589e0
fredrhaetw <frederico.rhae@thoughtworks.com>
2022-10-18 16:44:41 +0200

    Solving conflicts

--- AlignColumns/src/AlignColumns.java
+++ AlignColumns/src/AlignColumns.java
@@ -1,6 +1,35 @@
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+
 public class AlignColumns {
 
-    public static int answer() {
-        return 6 * 9;
+    public static String replaceDollarBySpace (String text) { 
+        String result = text.replace("$", " ");
+        return result.replace("  ", "$ ");
+    }
+
+    public static String align(String text) {
+        String textReplaced = replaceDollarBySpace(text);
+        var wordMatrix = createWordMatrix(textReplaced);
+
+        /*
+        1 - Renplazamos los dolares por espacio
+        2 - Creamos una matrix con cada palabra y cada línea
+        3 - Tenemos que buscar la palabra más grande por coluna
+        4 - Añadir espacio para se quedar de acuerdo a el length de la palabra más grande
+        5 - Generar una string con todas las líneas
+        * */
+
+        return "a";
+    }
+
+    public static List<String[]> createWordMatrix(String textReplaced) {
+        String[] allLines = textReplaced.split("\n");
+
+        return Stream.of(allLines)
+                .map(line -> line.split(" "))
+                .collect(Collectors.toList());
     }
 }
--- AlignColumns/test/AlignColumnsTest.java
+++ AlignColumns/test/AlignColumnsTest.java
@@ -2,8 +2,17 @@
 // JUnit assertion - the default Java assertion library
 // https://junit.org/junit5/
 
-import org.junit.jupiter.api.*;
-import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class AlignColumnsTest {
@@ -11,16 +20,85 @@ public class AlignColumnsTest {
     @Test
     void shouldReturnSameInputWhenDollarIsNotPresent() {
         // Given
-        var input = "ArturoTheDriver!";
+        var input = "Unacadenacambiada";
+        var expected = "Unacadenacambiada";
 
         // When
-        var output = AlignColumns.align(input);
+        var actual = AlignColumns.replaceDollarBySpace(input);
 
         // Then
-        
-
-        int expected = 42;
-        int actual = AlignColumns.answer();
         assertEquals(expected, actual);
     }
+
+    @Test
+    void shouldReturnTwoSeparatedWordsWhenDollarIsPresentInBetween() {
+        // Given
+        var input = "Cadena$cambiada";
+        var expected = "Cadena cambiada";
+
+        // When
+        var actual = AlignColumns.replaceDollarBySpace(input);
+
+        // Then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldReturnFiveSeparatedWordsWhenDollarIsPresentInBetween() {
+        // Given
+        var input = "Soy$una$cadena$de$cinco$palabras";
+        var expected = "Soy una cadena de cinco palabras";
+
+        // When
+        var actual = AlignColumns.replaceDollarBySpace(input);
+
+        // Then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldReturnTwoPhrasesAlignedByColumnsWhenThereIsABreakLine() {
+        // Given
+        var input = "Cadena$cambiada\nSoy$cadena";
+        var expected = "Cadena cambiada\nSoy    cadena";
+
+        // When
+        var actual = AlignColumns.align(input);
+
+        // Then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldReturnWordMatrixWithForAllTheLines() {
+        // Given
+        var input = "Cadena cambiada\nSoy cadena";
+        List<String[]> expected = Arrays.asList(new String[]{"Cadena", "cambiada"}, new String[]{"Soy","cadena"});
+
+        // When
+        var actual = AlignColumns.createWordMatrix(input);
+
+        // Then
+        Assertions.assertAll(
+                IntStream.range(0, actual.size())
+                        .mapToObj(i -> () -> Assertions.assertArrayEquals(expected.get(i),
+                                actual.get(i)))
+        );
+    }
+
+    @Test
+    void shouldReturnOneDollarSignWhenThereAreTwoDollarsContiguous() {
+        // Given
+        var input = "Cuesta$20$$todo";
+        var expected = "Cuesta 20$ todo";
+
+        // When
+        var actual = AlignColumns.replaceDollarBySpace(input);
+
+        // Then
+        assertEquals(expected, actual);
+    }
+
+
+
 }
