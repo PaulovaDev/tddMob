a71a58f55c7ffb149fab6b3e57313d7f5bb355c1
marta.ortuno <marta.ortuno@thoughtworks.com>
2022-10-18 15:37:29 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:AlignColumns/src/AlignColumns.java

--- AlignColumns/src/AlignColumns.java
+++ AlignColumns/src/AlignColumns.java
@@ -1,3 +1,5 @@
+import java.util.stream.Stream;
+
 import org.junit.platform.console.shadow.picocli.CommandLine.Help.Ansi.Text;
 
 public class AlignColumns {
@@ -10,7 +12,11 @@ public class AlignColumns {
     public static String align(String text) {
         String textReplaced = replaceDollarBySpace(text);
         String[] allLines = textReplaced.split("\n");
-
+        int cont = 0;
+        Stream.of(allLines).forEach(line -> {
+            
+        });
+        
         return "a";
     }
 }
